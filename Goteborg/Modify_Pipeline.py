4be790a1c470212c9bf79c40887827f29d1ed787
Jake Vogel <jacobwvogel@gmail.com>
2019-02-21 19:48:17 +0100

    defined a few dialog boxes

--- Goteborg/Modify_Pipeline.py
+++ Goteborg/Modify_Pipeline.py
@@ -1,2 +1,34 @@
 import os
 import tkinter as tk
+from tkinter import filedialog
+
+def close():
+    global l, root, selection
+    selection = [l.get(idx) for idx in l.curselection()]
+    root.destroy()
+
+def multiple_options(options):
+	print('WHAT DO YOU WANT TO CHANGE? SELECT ALL THAT APPLY')
+	root = tk.Tk()
+    l = tk.Listbox(root, width = 15,selectmode=tk.EXTENDED)
+    l.pack()
+    [l.insert(tk.END,x) for x in options]
+    b = tk.Button(root, text = "OK", command = close).pack()
+    root.mainloop()
+
+    return selection
+
+def select_file():
+	root = tk.Tk()
+    root.withdraw()
+
+    file_path = filedialog.askopenfilename()
+
+    return file_path
+
+print('WHAT DO YOU WANT TO CHANGE? SELECT ALL THAT APPLY')
+options = ['Inputs/Outputs','Extraction Atlas','PVC', 'Quantification', 'Appian']
+
+
+
+
