4e86c72e9fa1b4ce93e529b860d7027336264b0e
Jake Vogel <jacobwvogel@gmail.com>
2019-02-22 16:18:44 +0100

    added tested mnc2nii script

--- Goteborg/mnc2nii.py
+++ Goteborg/mnc2nii.py
@@ -12,25 +12,28 @@ dirname = filedialog.askdirectory(parent=root,title='Please select directory you
 if dirname == ():
     raise ValueError('No directory selected. Aborting!')
 
+print(dirname)
 ### PROMPT USER FOR RECURSIVE OR NOT
 response = input('Do you want to recursively convert every .mnc file in the tree? ')
 if response not in ('y','n','Y','N','Yes','No','yes','no'):
     raise IOError('Response not recognized')
-        if response in ('Yes','yes','y','Y'):
-            ans = True
-            hold = False
-        else:
-            ans = False
-            hold = False
+else:
+    if response in ('Yes','yes','y','Y'):
+        ans = True
+        hold = False
+    else:
+        ans = False
+        hold = False
 
 ### COLLECT ALL MNCs
 if ans:
     all_fls = []
-	for dirs, things, fls in os.walk(dirname):
-		if len(fls) > 0:
-			all_fls.append(fls)
+    for dirs, things, fls in os.walk(dirname):
+        if len(fls) > 0:
+            for fl in fls:
+                all_fls.append(os.path.join(dirs,fl))
 else:
-	all_fls = sorted(glob(dirname))
+    all_fls = sorted(glob(os.path.join(dirname,'*')))
 
 all_mncs = [x for x in all_fls if '.mnc' in x]
 
@@ -45,28 +48,33 @@ for mnc in all_mncs:
     	already_done.append(mnc)
 
 print('%s mncs already have a nifti version. Skipping these files...'%(len(already_done)))
-[mncs.remove(x) for x in already_done]
+[all_mncs.remove(x) for x in already_done]
+
+print('the following files will be converted:')
+[print(x) for x in all_mncs]
 
 ### PROMPT USER ABOUT DELETION
 response = input('Do you want to delete the mnc files? Not recommended unless low on space... ')
 if response not in ('y','n','Y','N','Yes','No','yes','no'):
     raise IOError('Response not recognized')
-        if response in ('Yes','yes','y','Y'):
-            ans = True
-            hold = False
-        else:
-            ans = False
-            hold = False
+else:        
+    if response in ('Yes','yes','y','Y'):
+       ans = True
+       hold = False
+    else:
+        ans = False
+        hold = False
 
 ### TRANSFORM FILES
-for mnc in mncs:
-	flnm = mnc.split('.')[0]
-	if mnc[-1] == 'z':
-		new_nm = '%s.nii.gz'%flnm
-	else:
-		new_nm = '%s.nii'%flnm
-	img = nib.load(mnc)
-	nifti = ni.Nifti1Image(img.get_data(), img.affine).to_filename(new_nm)
-	if ans:
-		os.remove(mnc)
+for mnc in all_mncs:
+    flnm = mnc.split('.')[0]
+    if mnc[-1] == 'z':
+        new_nm = '%s.nii.gz'%flnm
+    else:
+        new_nm = '%s.nii'%flnm
+    img = nib.load(mnc)
+    nifti = nib.Nifti1Image(img.get_data(), img.affine).to_filename(new_nm)
+    print('converted %s to %s'%(mnc,new_nm))
+    if ans:
+        os.remove(mnc)
 
